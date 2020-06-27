5f968f6777921c04112c3db52b291c139acba67a
do-kevin <do.kevin.vo@gmail.com>
2020-06-26 21:54:14 -0700

    Updates

---
+++ precache-manifest.c48db413fb9824cb2ae733cece1c25f8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4f364cd249976252e36fd680025225",
+    "url": "/react-rescripts-antd-starter-kit/index.html"
+  },
+  {
+    "revision": "4276c2ae3effe5d81c81",
+    "url": "/react-rescripts-antd-starter-kit/static/css/3.98904529.chunk.css"
+  },
+  {
+    "revision": "74d90993adf610954749",
+    "url": "/react-rescripts-antd-starter-kit/static/css/main.5bb9c07f.chunk.css"
+  },
+  {
+    "revision": "3017c7a1ca30bc078c63",
+    "url": "/react-rescripts-antd-starter-kit/static/js/0.9dfcfb75.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/react-rescripts-antd-starter-kit/static/js/0.9dfcfb75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4276c2ae3effe5d81c81",
+    "url": "/react-rescripts-antd-starter-kit/static/js/3.a3590f55.chunk.js"
+  },
+  {
+    "revision": "22e8e9a39f7f03a65d7e8ad154725a76",
+    "url": "/react-rescripts-antd-starter-kit/static/js/3.a3590f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "febcc4f9c9aa98df9537",
+    "url": "/react-rescripts-antd-starter-kit/static/js/4.afd77873.chunk.js"
+  },
+  {
+    "revision": "a15c173efb2c7ecac23e",
+    "url": "/react-rescripts-antd-starter-kit/static/js/5.b2b3bbd1.chunk.js"
+  },
+  {
+    "revision": "279f185f9f0e2391d0b4",
+    "url": "/react-rescripts-antd-starter-kit/static/js/6.e7a0b005.chunk.js"
+  },
+  {
+    "revision": "f196195fcf9e8ba25899",
+    "url": "/react-rescripts-antd-starter-kit/static/js/7.248ff93c.chunk.js"
+  },
+  {
+    "revision": "cadce0a40632a04634ba",
+    "url": "/react-rescripts-antd-starter-kit/static/js/8.ea3084a3.chunk.js"
+  },
+  {
+    "revision": "74d90993adf610954749",
+    "url": "/react-rescripts-antd-starter-kit/static/js/main.525a8074.chunk.js"
+  },
+  {
+    "revision": "7037c84dbdf27fe953df",
+    "url": "/react-rescripts-antd-starter-kit/static/js/runtime-main.e7928d63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-rescripts-antd-starter-kit/static/media/logo.5d5d9eef.svg"
+  }
+]);
